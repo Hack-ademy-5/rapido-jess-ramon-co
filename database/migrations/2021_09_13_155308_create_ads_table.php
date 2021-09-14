f0cc0325b3624e10c2c8ffa405f1f6b6c04c0387
Jessica J Montero <holajjmontero@gmail.com>
2021-09-14 10:02:00 +0200

    start project

---
+++ database/migrations/2021_09_13_155308_create_ads_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ads', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title');
+            $table->unsignedBigInteger('price');
+            $table->text('description');
+            $table->string('category');
+
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ads');
+    }
+}
