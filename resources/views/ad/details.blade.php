edf84a0bcb1e7684f12e28d970f8645f8c7f0883
ramon <ramonmartinezf@gmail.com>
2021-09-15 15:33:59 +0200

    story2 completada

--- resources/views/ad/details.blade.php
+++ resources/views/ad/details.blade.php
@@ -8,10 +8,26 @@
 <div class="alert alert-success">{{session('ad.create.success')}}</div>
 @endif
 
-<div class="container my-5">
-    <div class="row"> 
-            <h1>Vista de anuncio</h1>
-            @include('ad._ad') 
+
+<div class="row"> 
+    <h1>Nombre del anuncio: {{$ad->title}}</h1>       
+</div>
+
+<div class="row">
+    <div class="col-12 col-md-4">
+        <div class="card mb-5" style="width: 18rem;">
+            <img src="https://via.placeholder.com/150" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">{{$ad->title}}</h5>
+              <h6 class="card-subtitle mb-2 text-muted">{{$ad->price}}</h6>
+              <p class="card-text"> {{$ad->body}}</p>
+              <h6 class="card-subtitle mb-2">
+                <strong>Categoria: <a href="{{route('category.ads',['name'=>$ad->category->name,'id'=>$ad->category->id])}}">{{$ad->category->name}}</a></strong>
+            <i>{{$ad->created_at->format('d/m/Y')}} - {{$ad->user->name }}</i></h6>
+            <a href="{{route("ad.details", ['id'=>$ad->id])}}">Leer más</a>
+              <a href="#" class="card-link">Link</a>
+            </div>
+        </div>
     </div>
 </div>
 
