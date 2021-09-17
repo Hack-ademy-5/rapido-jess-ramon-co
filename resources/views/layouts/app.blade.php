cfdfb9c33b978e0171f79baf5b527fd17a2994f6
Jessica J Montero <holajjmontero@gmail.com>
2021-09-17 16:43:19 +0200

    frontend /ad/new, queda por pulir algunos detalles de pading entre los <div>

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
         <link rel="stylesheet" href="{{mix('css/app.css')}}">
         <!-- el link Lavavel MIX | primero copila resouce >sass > app.scss  llama a public > css > app.css (y ya renderiza nuestra página)-->
 
-    <title>rapidoEs - @yield('title')</title>
+    <title>rapidoEs · @yield('title')</title>
 </head>
 
 <body>
@@ -21,13 +21,18 @@
         <div class="alert alert-danger">{{session('access.denied.revisor.only')}}</div>
     @endif
 
+    <!-- Barra de navegación -->
     @include('layouts._nav')
 
+    <!-- distribuiremos los <div> de los últimos 6 anuncios  |  <section> <article> 1 </article> </section>  | -->
     <div class="container"></div>
     @yield('content')
     </div>
 
+    <!-- llamada a la barra de footer -->
     @include('layouts._footer')
+
+
 
     <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous">
